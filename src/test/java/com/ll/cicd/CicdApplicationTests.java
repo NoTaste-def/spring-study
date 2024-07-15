1e4eadfdf5867165c7537bf883ef44ae006e66df
NoTaste-def <ddrnjs1245@gmail.com>
2024-07-15 16:59:52 +0900

    day 1

--- src/test/java/com/ll/cicd/CicdApplicationTests.java
+++ src/test/java/com/ll/cicd/CicdApplicationTests.java
@@ -1,18 +1,16 @@
 package com.ll.cicd;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+
+//	@Autowired
+//	private AnswerRepository answerRepository;
 
 //	@Test
 //	// 데이터베이스에 질문 저장
@@ -73,11 +71,63 @@ class CicdApplicationTests {
 //		assertEquals("sbb가 무엇인가요?", q.getSubject());
 //	}
 
-	@Test
-	void testJpa() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
+//	@Test
+//	// Modify Question Data
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		q.setSubject("수정된 제목");
+//		this.questionRepository.save(q);
+//	}
+
+//	@Test
+//	// Delete Question Data
+//	void testJpa() {
+//		assertEquals(2, this.questionRepository.count());
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		this.questionRepository.delete(q);
+//		assertEquals(1, this.questionRepository.count());
+//	}
+
+//	@Test
+//	// Create Answer Data
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		Answer a = new Answer();
+//		a.setContent("네 자동으로 생성됩니다.");
+//		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+//	}
+
+//	@Test
+//	// Read Answer Data
+//	void testJpa() {
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer a = oa.get();
+//		assertEquals(2, a.getQuestion().getId());
+//	}
+
+//	@Test
+//	// 테스트 환경에서는 에러가 남.  Question 객체를 조회하고 나면 DB 세션이 종료되기 때문. 실제 환경에서는 종료되지 않음.
+//	// Find answer data from question data
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		List<Answer> answerList = q.getAnswerList();
+//
+//		assertEquals(1, answerList.size());
+//		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//	}
+
 
 }
